2037dc41cc95aeb93b40268fdc5f533c2f9c37c7
kstee <kstee_90@hotmail.com>
2024-12-08 18:27:37 +0800

    Completed parity deals clone

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,71 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { env } from "@/data/env/server";
+import {
+  createUserSubscription,
+  getUserSubscription,
+} from "@/server/db/subscription";
+import { deleteUser } from "@/server/db/users";
+import Stripe from "stripe";
+
+const stripe = new Stripe(env.STRIPE_SECRET_KEY);
+
+export async function POST(req: Request) {
+  const headerPayload = await headers();
+  const svixId = headerPayload.get("svix-id");
+  const svixTimestamp = headerPayload.get("svix-timestamp");
+  const svixSignature = headerPayload.get("svix-signature");
+
+  if (!svixId || !svixTimestamp || !svixSignature) {
+    return new Response("Error occurred -- no svix headers", {
+      status: 400,
+    });
+  }
+
+  // Get the body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(env.CLERK_WEBHOOK_SECRET);
+
+  let event: WebhookEvent;
+
+  // Verify the payload with the headers
+  try {
+    event = wh.verify(body, {
+      "svix-id": svixId,
+      "svix-timestamp": svixTimestamp,
+      "svix-signature": svixSignature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
+      status: 400,
+    });
+  }
+
+  switch (event.type) {
+    case "user.created": {
+      await createUserSubscription({
+        clerkUserId: event.data.id,
+        tier: "Free",
+      });
+      break;
+    }
+    case "user.deleted": {
+      if (event.data.id != null) {
+        const userSubscription = await getUserSubscription(event.data.id);
+        if (userSubscription?.stripeSubscriptionId != null) {
+          await stripe.subscriptions.cancel(
+            userSubscription?.stripeSubscriptionId
+          );
+        }
+        await deleteUser(event.data.id);
+      }
+    }
+  }
+
+  return new Response("", { status: 200 });
+}
